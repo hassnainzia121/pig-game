90f8ccfe325c885d9fc2f8302fd8aacea395816e
Hassnain Zia <168231214+hassnainzia121@users.noreply.github.com>
2025-09-09 12:06:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ btnHold.addEventListener('click', () => {
     document.getElementById(`score--${activePlayer}`).textContent =
       score[activePlayer];
 
-    if (score[activePlayer] >= 20) {
+    if (score[activePlayer] >= 100) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
